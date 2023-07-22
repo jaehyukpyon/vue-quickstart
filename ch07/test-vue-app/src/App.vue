6076db48eda3697f234c4e2239d21ae0e37b1a46
jaehyuk <jaehyuk.pyon@gmail.com>
2023-07-22 23:21:12 +0900

    vue with vite

---
+++ ch07/test-vue-app/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+      <h2>K-POP?</h2>
+      <hr>
+      <ul>
+        <!-- <CheckboxItem v-for="idol in idols" :key="idol.id" :name="idol.name" :checked="idol.checked" /> -->
+        <!-- <checkbox-item v-for="idol in idols" :key="idol.id" v-bind="idol" /> -->
+        <CheckboxItem v-for="idol in idols" :key="idol.id" :idol="idol" />
+      </ul>
+  </div>
+</template>
+
+<script>
+  import CheckboxItem from './components/CheckboxItem.vue';
+
+  export default {
+    name: "App",
+    components: {
+      CheckboxItem,
+    },
+    data() {
+      return {
+        idols: [
+          { id: 1, name: "BTS", checked: true },
+          { id: 2, name: "Black Pink", checked: false },
+          { id: 3, name: "EXO", checked: false },
+          { id: 4, name: "ITZY", checked: false },
+        ]
+      };
+    },
+    beforeCreate() {
+      console.log("App.vue beforeCreate()");
+    },
+    created() {
+      console.log("App.vue created()");
+    },
+    beforeMount() {
+      console.log("App.vue beforeMount()");
+    },
+    mounted() {
+      console.log("App.vue mounted()");
+    },
+    beforeUpdate() {
+      console.log("App.vue beforeUpdate()");
+    },
+    updated() {
+      console.log("App.vue updated()");
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ ch07/test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <input type="checkbox" :checked="idol.checked">{{idol.name}}
+  </li>
+</template>
+
+<script>
+  export default {
+    name: "CheckboxItem",
+    props: [
+      'idol',
+    ],
+    data() {
+      return {
+        
+      };
+    },
+    beforeCreate() {
+      console.log("CheckboxItem.vue beforeCreate()");
+    },
+    created() {
+      console.log("CheckboxItem.vue created()");
+    },
+    beforeMount() {
+      console.log("CheckboxItem.vue beforeMount()");
+    },
+    mounted() {
+      console.log("CheckboxItem.vue mounted()");
+    },
+    beforeUpdate() {
+      console.log("CheckboxItem.vue beforeUpdate()");
+    },
+    updated() {
+      console.log("CheckboxItem.vue updated()");
+    },
+  }
+</script>
+
+<style scoped></style>
